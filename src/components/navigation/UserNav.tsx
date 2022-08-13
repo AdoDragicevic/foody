73fe711ed7572c15fde98a6429fdcd93268a53a7
Ado Dragicevic <adodragicevic@gmail.com>
2022-08-13 23:17:44 +0200

    style user-nav

--- src/components/navigation/UserNav.tsx
+++ src/components/navigation/UserNav.tsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import { FaListUl } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaUserAlt } from "react-icons/fa";
+
 
 const UserNav = () => (
   <nav className="user-nav">
-    <Link to="/profile" className="user-nav__link">
-      Profile
+    <Link to="/profile" className="user-nav__link btn">
+      <FaUserAlt />
+      <span className="user-nav__tag">Profile</span>
     </Link>
-    <Link to="/orders" className="user-nav__link">
-      Orders
+    <Link to="/orders" className="user-nav__link btn">
+      <FaListUl />
+      <span className="user-nav__tag">Orders</span>
     </Link>
-    <Link to="/cart" className="user-nav__link">
-      Cart
+    <Link to="/cart" className="user-nav__link btn">
+      <FaShoppingCart />
+      <span className="user-nav__tag">Cart</span>
     </Link>
   </nav>
 );
